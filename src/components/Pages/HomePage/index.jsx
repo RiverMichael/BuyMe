fae8e9fd80729cd2e2c8ee4f35ae7de9c94cf7c0
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-22 00:38:45 +0100

    Adds HomePage component

---
+++ src/components/Pages/HomePage/index.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      {/* // SearchBar component will be added here 
+      // ProductsList component will be added here */}
+    </>
+  );
+}
