016b11dffd88aedb54781138344931aaf2526712
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-22 00:46:29 +0100

    Adds CheckoutSuccessPage component

---
+++ src/components/Pages/CheckoutSuccessPage/index.jsx
@@ -0,0 +1,8 @@
+export default function CheckoutSuccessPage() {
+  return (
+    <>
+      <h1>Checkout Success Page</h1>
+      {/* CheckoutSuccess validation component goes here */}
+    </>
+  );
+}
