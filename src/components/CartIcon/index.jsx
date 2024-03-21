76b25f475891379fec234d6b5ae8f1fc59374724
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-21 23:10:26 +0100

    Creates CartIcon component

---
+++ src/components/CartIcon/index.jsx
@@ -0,0 +1,9 @@
+import { IoCartOutline } from "react-icons/io5";
+
+export default function CartIcon() {
+  return (
+    <button>
+      <IoCartOutline size={30} className="hover:text-sky-blue" />
+    </button>
+  );
+}
