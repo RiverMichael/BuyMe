29aa48e26653631da730f7ba28c99a81d3ba9b05
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-21 23:11:06 +0100

    Edits colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "node_modules/flowbite-react/lib/esm/**/*.js"],
   theme: {
     extend: {
+      colors: {
+        "ghost-white": "#f7f7f7",
+        "snow-mist": "#fbfbfb",
+        "sky-blue": "#87ceeb",
+        "navy-blue": "#003366",
+        "lavender-gray": "#e6e6f1",
+        "dark-gray": "#a9a9a9",
+        "silver-chalice": "#d3d3d3",
+        "gunmetal-gray": "#2a3439",
+        "jaguar-gray": "#292929",
+        "crimson-red": "#dc143c",
+      },
       fontFamily: {
         heading: ["Montserrat", "sans-serif"],
         body: ["Lato", "sans-serif"],
