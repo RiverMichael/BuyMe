e553d5d645577733732dd686ddea56e558082cfc
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-22 00:45:17 +0100

    Adds CheckoutPage component

---
+++ src/components/Pages/CheckoutPage/index.jsx
@@ -0,0 +1,9 @@
+export default function CheckoutPage() {
+  return (
+    <>
+      <h1>Checkout Page</h1>
+      {/* Cart component goes here */
+      /* Checkout button goes here */}
+    </>
+  );
+}
