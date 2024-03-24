aeff3cf64b81628ac0d26e914bd421de24534bdf
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-24 20:02:05 +0100

    Changes styling on NavBar to use DaisyUi

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -2,8 +2,11 @@ import { IoCartOutline } from "react-icons/io5";
 
 export default function CartIcon() {
   return (
-    <button className="flex items-center">
-      <IoCartOutline size={30} />
+    <button className="group btn btn-ghost p-0 hover:text-primary hover:scale-110 hover:-rotate-12 transition-all duration-300 ease-in-out">
+      <div className="indicator">
+        <IoCartOutline size={30} />
+        <span className="badge indicator-item bg-primary border-none group-hover:text-snow-mist ">2</span>
+      </div>
     </button>
   );
 }
