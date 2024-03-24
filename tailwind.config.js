e39fc419886f37009d3f8e297d077f8ef74857fc
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-24 17:52:16 +0100

    Deletes Flowbite and adds DaisyUi

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "node_modules/flowbite-react/lib/esm/**/*.js"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
@@ -21,5 +21,5 @@ export default {
       },
     },
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [require("daisyui")],
 };
