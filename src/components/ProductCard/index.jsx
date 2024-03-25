9ac6bac494b1472d821faae098a071789d373884
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-25 19:30:04 +0100

    Styling changes on ProductCard

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -9,25 +9,25 @@ export default function ProductCard({ product }) {
     <div className="card card-compact w-96 bg-ghost-white shadow-lg ms-5">
       <img src={product.image.url} alt={product.image.alt} className="rounded-t-box" />
 
-      <div className="card-body">
+      <div className="card-body gap-2.5">
         <div className="flex flex-col">
           <h2 className="card-title items-start">
             {product.title}
             <CalculateDiscount price={product.price} discountedPrice={product.discountedPrice} />
           </h2>
-          <div className="flex gap-2 text-dark-gray">
+          <div className="flex gap-2.5 text-dark-gray">
             <StarRating rating={product.rating} />
             <span>|</span>
             <Reviews id={product.id} reviews={product.reviews} />
           </div>
         </div>
 
-        <div className="flex gap-2 items-center justify-start">
+        <div className="flex gap-3 items-center justify-start">
           <DisplayPrice price={product.price} discountedPrice={product.discountedPrice} />
         </div>
 
-        <div className="card-actions justify-end">
-          <Link to={`/products/${product.id}`} className="btn btn-sm bg-gunmetal-gray text-ghost-white hover:border-gunmetal-gray hover:text-gunmetal-gray">
+        <div className="card-actions justify-center">
+          <Link to={`/products/${product.id}`} className="btn btn-sm btn-wide bg-gunmetal-gray text-ghost-white hover:border-gunmetal-gray hover:text-gunmetal-gray">
             View product
           </Link>
         </div>
