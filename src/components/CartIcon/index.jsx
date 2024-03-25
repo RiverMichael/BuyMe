a81cb703adb0b7855e988677584ed8472689e657
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-25 23:19:21 +0100

    Updates the CartIcon badge based on the number of items in the cart

--- src/components/CartIcon/index.jsx
+++ src/components/CartIcon/index.jsx
@@ -1,11 +1,14 @@
 import { IoCartOutline } from "react-icons/io5";
+import productStore from "../store/products";
 
 export default function CartIcon() {
+  const { getTotalNumberOfItemsInCart } = productStore();
+
   return (
     <button className="group btn btn-ghost p-0 hover:text-primary hover:scale-110 hover:-rotate-12 transition-all duration-300 ease-in-out">
       <div className="indicator">
         <IoCartOutline size={30} />
-        <span className="badge indicator-item bg-primary border-none group-hover:text-snow-mist">0</span>
+        {getTotalNumberOfItemsInCart() ? <span className="badge indicator-item bg-primary border-none group-hover:text-snow-mist">{getTotalNumberOfItemsInCart()}</span> : ""}
       </div>
     </button>
   );
