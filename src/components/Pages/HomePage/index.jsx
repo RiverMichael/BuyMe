eeec7ec940d483cd1996f5c4eb8ec3d6ae9700e6
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-26 13:54:32 +0100

    Creates ProductsList component and displays the products on the HomePage

--- src/components/Pages/HomePage/index.jsx
+++ src/components/Pages/HomePage/index.jsx
@@ -1,9 +1,12 @@
+import ProductsList from "../../ProductsList";
+
 export default function HomePage() {
   return (
-    <>
-      <h1>Home Page</h1>
-      {/* // SearchBar component will be added here 
-      // ProductsList component will be added here */}
-    </>
+    <main className="mt-5 flex flex-col gap-y-10">
+      {/* SearchBar component will be added here */}
+      <div className="container mx-auto "></div>
+      <h1 className="text-center">Products</h1>
+      <ProductsList />
+    </main>
   );
 }
---
+++ src/components/ProductsList/index.jsx
@@ -0,0 +1,26 @@
+import { API_BASE_URL } from "../../shared/api";
+import ProductCard from "../ProductCard";
+import useFetch from "../Hooks/useFetch";
+
+export default function ProductsList() {
+  const { data, isLoading, isError } = useFetch(API_BASE_URL);
+  const products = data.data;
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return <div>Oops! Something went wrong, please try again</div>;
+  }
+
+  if (products) {
+    return (
+      <div className="flex flex-wrap gap-y-10 gap-x-5 justify-evenly 2xl:container mx-auto">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    );
+  }
+}
