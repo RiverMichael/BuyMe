d295f4e6e72409fa9dc7324a0bb74e0cacb6a3e4
Michael River Nilsson <mrn@michaelriver.dev>
2024-03-31 20:09:18 +0200

    Changes prices to be fixed to two decimals

--- src/components/DisplayPrice/index.jsx
+++ src/components/DisplayPrice/index.jsx
@@ -1,14 +1,14 @@
 const DisplayPrice = ({ price, discountedPrice }) => {
   if (!discountedPrice || discountedPrice >= price) {
-    return <p className="text-lg font-bold">${price}</p>;
+    return <p className="text-lg font-bold">${price.toFixed(2)}</p>;
   } else if (!price) {
-    return <p className="text-lg font-bold">${discountedPrice}</p>;
+    return <p className="text-lg font-bold">${discountedPrice.toFixed(2)}</p>;
   }
 
   return (
     <>
-      <p className="line-through flex-none">${price}</p>
-      <p className="text-lg font-bold text-accent">${discountedPrice}</p>
+      <p className="line-through flex-none">${price.toFixed(2)}</p>
+      <p className="text-lg font-bold text-accent">${discountedPrice.toFixed(2)}</p>
     </>
   );
 };
