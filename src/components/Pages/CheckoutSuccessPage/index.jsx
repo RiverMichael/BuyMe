a9bae89a59188b0520e314048ff5380ecfbc9d2f
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-03 19:50:52 +0200

    Changes path and routing to CheckoutPage

--- src/components/Pages/CheckoutSuccessPage/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function CheckoutSuccessPage() {
-  return (
-    <>
-      <h1>Checkout Success Page</h1>
-      {/* CheckoutSuccess validation component goes here */}
-    </>
-  );
-}
