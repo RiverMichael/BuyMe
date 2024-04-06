e85af8051900b81f5b8e56f5f66dc3a526cdb89c
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-06 16:39:54 +0200

    Creates ContactForm and displays it on the ContactPage

---
+++ src/components/ContactForm/index.jsx
@@ -0,0 +1,112 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { IoPerson, IoMail, IoPencil, IoChatboxEllipses } from "react-icons/io5";
+
+const schema = yup.object({
+  fullName: yup.string().min(3, "Full name must be at least 3 characters").required(),
+  email: yup.string().email("Please enter a valid email address").required("Please enter a valid email address"),
+  subject: yup.string().min(3, "Subject must be at least 3 characters").required(),
+  body: yup.string().min(3, "Message must be at least 3 characters").required(),
+});
+
+export default function ContactForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(schema), mode: "onChange" });
+
+  function onSubmit(data) {
+    console.log("Contact form data:", data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="form-control items-center gap-5">
+      <div className="form-control w-full gap-1">
+        <label htmlFor="fullName" className="label w-full py-0">
+          <span className="flex items-center gap-2">
+            <IoPerson aria-hidden="true" />
+            Full name
+          </span>
+          <span className="label-text-alt font-light">Required</span>
+        </label>
+        <input
+          id="fullName"
+          {...register("fullName")}
+          className={`input input-bordered rounded grow ${
+            errors.fullName ? "border-red-500 focus:border-red-500" : "border-silver-chalice"
+          } hover:border-gunmetal-gray focus:outline-none focus:border-2`}
+          placeholder="Firstname Lastname"
+        />
+        <p className="text-red-500 text-sm" aria-live="polite">
+          {errors.fullName?.message}
+        </p>
+      </div>
+
+      <div className="form-control w-full gap-1">
+        <label htmlFor="email" className="label w-full py-0">
+          <span className="flex items-center gap-2">
+            <IoMail aria-hidden="true" />
+            Email
+          </span>
+          <span className="label-text-alt font-light">Required</span>
+        </label>
+        <input
+          type="email"
+          id="email"
+          {...register("email")}
+          className={`input input-bordered rounded grow ${errors.email ? "border-red-500 focus:border-red-500" : "border-silver-chalice"} hover:border-gunmetal-gray focus:outline-none focus:border-2`}
+          placeholder="name@mail.com"
+        />
+        <p className="text-red-500 text-sm" aria-live="polite">
+          {errors.email?.message}
+        </p>
+      </div>
+
+      <div className="form-control w-full gap-1">
+        <label htmlFor="subject" className="label w-full py-0">
+          <span className="flex items-center gap-2">
+            <IoPencil aria-hidden="true" />
+            Subject
+          </span>
+          <span className="label-text-alt font-light">Required</span>
+        </label>
+        <input
+          id="subject"
+          {...register("subject")}
+          className={`input input-bordered rounded grow ${
+            errors.subject ? "border-red-500 focus:border-red-500" : "border-silver-chalice"
+          } hover:border-gunmetal-gray focus:outline-none focus:border-2`}
+          placeholder="Write the subject here"
+        />
+        <p className="text-red-500 text-sm" aria-live="polite">
+          {errors.subject?.message}
+        </p>
+      </div>
+
+      <div className="form-control w-full gap-1">
+        <label htmlFor="message" className="label w-full py-0">
+          <span className="flex items-center gap-2">
+            <IoChatboxEllipses aria-hidden="true" /> Message
+          </span>
+          <span className="label-text-alt font-light">Required</span>
+        </label>
+        <textarea
+          cols={30}
+          rows={5}
+          id="message"
+          {...register("body")}
+          className={`textarea textarea-bordered text-base rounded grow ${
+            errors.body ? "border-red-500 focus:border-red-500" : "border-silver-chalice"
+          } hover:border-gunmetal-gray focus:outline-none focus:border-2`}
+          placeholder="Write your message here"></textarea>
+        <p className="text-red-500 text-sm" aria-live="polite">
+          {errors.body?.message}
+        </p>
+      </div>
+
+      <button className="btn btn-wide bg-gunmetal-gray text-ghost-white hover:text-gunmetal-gray">Send message</button>
+    </form>
+  );
+}
--- src/components/Pages/ContactPage/index.jsx
+++ src/components/Pages/ContactPage/index.jsx
@@ -1,8 +1,16 @@
+import ContactForm from "../../ContactForm";
+
 export default function ContactPage() {
   return (
     <>
-      <h1>Contact Page</h1>
-      {/* ContactForm component goes here */}
+      <main className="mt-10 mb-20 container mx-auto items-center flex flex-col gap-y-10">
+        <div className="flex flex-col items-center gap-5 max-w-sm">
+          <h1>Contact us</h1>
+          <p className="text-center">Please send us a message if you have any questions or concerns about our products.</p>
+        </div>
+
+        <ContactForm />
+      </main>
     </>
   );
 }
