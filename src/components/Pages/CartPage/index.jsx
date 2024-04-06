b97290059edb747f0f82dbc8528c223407a44383
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-06 19:19:02 +0200

    Styling changes on the cart page

--- src/components/Pages/CartPage/index.jsx
+++ src/components/Pages/CartPage/index.jsx
@@ -5,7 +5,8 @@ export default function CartPage() {
   useDocumentTitle("Checkout");
 
   return (
-    <main className="mt-10 mb-20 mx-auto px-5">
+    <main className="mt-10 mb-20 mx-auto px-5 flex flex-col gap-12">
+      <h1 className="text-center">Shopping cart</h1>
       <ShoppingCart />
     </main>
   );
--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -24,119 +24,115 @@ export default function ShoppingCart() {
   }
 
   return (
-    <div className="flex flex-col gap-12">
-      <h1 className="text-center">Checkout</h1>
-
-      <section className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-24 justify-center">
-        <div className="flex flex-col gap-5">
-          <div className="flex gap-5 justify-between">
-            <h2 className="text-2xl">Shopping cart</h2>
-            {cart.length ? (
-              <div>
-                <button onClick={() => handleClearCart()} className="btn btn-xs btn-outline rounded hover:bg-gunmetal-gray">
-                  Clear cart
-                </button>
-              </div>
-            ) : (
-              ""
-            )}
-          </div>
-
+    <section className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-24 justify-center">
+      <div className="flex flex-col gap-5">
+        <div className="flex gap-5 justify-between">
+          <h2 className="text-2xl">Your products</h2>
           {cart.length ? (
-            <ul className="flex flex-col gap-3">
-              {cart.map((item) => (
-                <li key={item.id} className="flex border-y py-1 gap-5 justify-between">
-                  <div className="flex gap-5">
-                    <div className="flex flex-col items-center gap-3">
-                      <div className="avatar w-24">
-                        <div className="w-full rounded">
-                          <Link to={`/products/${item.id}`}>
-                            <img src={item.image.url} alt={item.title} />
-                          </Link>
-                        </div>
-                      </div>
-
-                      <div className="flex w-full justify-between items-center">
-                        <button onClick={() => handleDecreaseProductQuantity(item)} className="btn btn-sm btn-outline rounded hover:bg-gunmetal-gray">
-                          -
-                        </button>
-                        {item.quantity}
-                        <button onClick={() => handleIncreaseProductQuantity(item)} className="btn btn-sm btn-outline rounded  hover:bg-gunmetal-gray">
-                          +
-                        </button>
-                      </div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <Link to={`/products/${item.id}`} className="hover:opacity-60">
-                        <h3 className="text-base">{item.title}</h3>
-                      </Link>
-
-                      <div className="flex flex-col">
-                        <DisplayDiscountPercent price={item.price} discountedPrice={item.discountedPrice} />
-                        <div className="flex flex-row-reverse gap-2 items-center justify-end">
-                          <DisplayPrice price={item.price * item.quantity} discountedPrice={item.discountedPrice * item.quantity} />
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="flex items-center">
-                    <button onClick={() => handleDeleteItem(item)} className="btn btn-sm btn-outline border-none hover:bg-gunmetal-gray">
-                      <IoTrashOutline size={15} />
-                    </button>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          ) : (
             <div>
-              <p>No items in the shopping cart</p>
+              <button onClick={() => handleClearCart()} className="btn btn-xs btn-outline rounded hover:bg-gunmetal-gray">
+                Clear cart
+              </button>
             </div>
+          ) : (
+            ""
           )}
         </div>
 
-        <div className="flex flex-col gap-5">
-          <h2 className="text-2xl">Total</h2>
+        {cart.length ? (
+          <ul className="flex flex-col gap-3">
+            {cart.map((item) => (
+              <li key={item.id} className="flex border-y py-1 gap-5 justify-between">
+                <div className="flex gap-5">
+                  <div className="flex flex-col items-center gap-3">
+                    <div className="avatar w-24">
+                      <div className="w-full rounded">
+                        <Link to={`/products/${item.id}`}>
+                          <img src={item.image.url} alt={item.title} />
+                        </Link>
+                      </div>
+                    </div>
 
-          <div className="flex flex-col gap-3">
-            {getTotalDiscount() ? (
-              <div className="flex flex-col gap 1">
-                <p className="flex justify-between">
-                  Price <span>${getTotalOriginalPrice().toFixed(2)}</span>
-                </p>
+                    <div className="flex w-full justify-between items-center">
+                      <button onClick={() => handleDecreaseProductQuantity(item)} className="btn btn-sm btn-outline rounded hover:bg-gunmetal-gray">
+                        -
+                      </button>
+                      {item.quantity}
+                      <button onClick={() => handleIncreaseProductQuantity(item)} className="btn btn-sm btn-outline rounded  hover:bg-gunmetal-gray">
+                        +
+                      </button>
+                    </div>
+                  </div>
+                  <div className="flex flex-col gap-2">
+                    <Link to={`/products/${item.id}`} className="hover:opacity-60">
+                      <h3 className="text-base">{item.title}</h3>
+                    </Link>
 
-                <p className="flex justify-between">
-                  Discount <span className="text-accent">- ${getTotalDiscount().toFixed(2)}</span>
-                </p>
-              </div>
-            ) : (
-              ""
-            )}
+                    <div className="flex flex-col">
+                      <DisplayDiscountPercent price={item.price} discountedPrice={item.discountedPrice} />
+                      <div className="flex flex-row-reverse gap-2 items-center justify-end">
+                        <DisplayPrice price={item.price * item.quantity} discountedPrice={item.discountedPrice * item.quantity} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
 
-            {getTotalNumberOfItemsInCart() ? (
-              <div className="flex flex-col">
-                <p className="flex justify-between">
-                  {getTotalNumberOfItemsInCart()} {getTotalNumberOfItemsInCart() > 1 ? "items" : "item"} <span>${getCartTotal().toFixed(2)}</span>
-                </p>
-                <p className="flex justify-between">
-                  Delivery <span>Free</span>
-                </p>
-              </div>
-            ) : (
-              ""
-            )}
-            <hr />
-            <h3 className="text-base flex justify-between">
-              Total <span>${getCartTotal().toFixed(2)}</span>
-            </h3>
+                <div className="flex items-center">
+                  <button onClick={() => handleDeleteItem(item)} className="btn btn-sm btn-outline border-none hover:bg-gunmetal-gray">
+                    <IoTrashOutline size={15} />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <div>
+            <p>No items in the shopping cart</p>
           </div>
-          <div className="flex justify-center">
-            <Link to="/checkout" className="btn btn-wide bg-gunmetal-gray text-ghost-white hover:text-gunmetal-gray" disabled={!cart.length}>
-              Checkout
-            </Link>
-          </div>
+        )}
+      </div>
+
+      <div className="flex flex-col gap-5">
+        <h2 className="text-2xl">Total</h2>
+
+        <div className="flex flex-col gap-3">
+          {getTotalDiscount() ? (
+            <div className="flex flex-col gap 1">
+              <p className="flex justify-between">
+                Price <span>${getTotalOriginalPrice().toFixed(2)}</span>
+              </p>
+
+              <p className="flex justify-between">
+                Discount <span className="text-accent">- ${getTotalDiscount().toFixed(2)}</span>
+              </p>
+            </div>
+          ) : (
+            ""
+          )}
+
+          {getTotalNumberOfItemsInCart() ? (
+            <div className="flex flex-col">
+              <p className="flex justify-between">
+                {getTotalNumberOfItemsInCart()} {getTotalNumberOfItemsInCart() > 1 ? "items" : "item"} <span>${getCartTotal().toFixed(2)}</span>
+              </p>
+              <p className="flex justify-between">
+                Delivery <span>Free</span>
+              </p>
+            </div>
+          ) : (
+            ""
+          )}
+          <hr />
+          <h3 className="text-base flex justify-between">
+            Total <span>${getCartTotal().toFixed(2)}</span>
+          </h3>
         </div>
-      </section>
-    </div>
+        <div className="flex justify-center">
+          <Link to="/checkout" className="btn btn-wide bg-gunmetal-gray text-ghost-white hover:text-gunmetal-gray" disabled={!cart.length}>
+            Checkout
+          </Link>
+        </div>
+      </div>
+    </section>
   );
 }
