1bd8faed75e0aa387e461ebb8ca6d9692c261e61
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-07 22:58:07 +0200

    Changes meta description on HomePage

--- src/components/Pages/HomePage/index.jsx
+++ src/components/Pages/HomePage/index.jsx
@@ -2,7 +2,7 @@ import useUpdateHead from "../../Hooks/useUpdateHead";
 import ProductsList from "../../ProductsList";
 
 export default function HomePage() {
-  useUpdateHead("Home", "Explore this amazing products that you have been looking for at BuyMe");
+  useUpdateHead("Home", "Explore amazing products that you have been looking for at BuyMe");
 
   return (
     <main className="mt-10 mb-20 container mx-auto flex flex-col gap-y-10">
