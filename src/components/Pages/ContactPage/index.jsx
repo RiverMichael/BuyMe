f28e58baa295095d5e5647ba9d6e8c9631791050
Michael River Nilsson <mrn@michaelriver.dev>
2024-04-07 13:59:51 +0200

    Removes unnessesary DOM elements

--- src/components/Pages/ContactPage/index.jsx
+++ src/components/Pages/ContactPage/index.jsx
@@ -5,15 +5,13 @@ export default function ContactPage() {
   useUpdateHead("Contact", "Feel free to contact us at BuyMe if you have any questions");
 
   return (
-    <>
-      <main className="mt-10 mb-20 container mx-auto items-center flex flex-col gap-y-10">
-        <div className="flex flex-col items-center gap-5 max-w-sm">
-          <h1>Contact us</h1>
-          <p className="text-center">Please send us a message if you have any questions or concerns about our products.</p>
-        </div>
+    <main className="mt-10 mb-20 container mx-auto items-center flex flex-col gap-y-10">
+      <div className="flex flex-col items-center gap-5 max-w-sm">
+        <h1>Contact us</h1>
+        <p className="text-center">Please send us a message if you have any questions or concerns about our products.</p>
+      </div>
 
-        <ContactForm />
-      </main>
-    </>
+      <ContactForm />
+    </main>
   );
 }
